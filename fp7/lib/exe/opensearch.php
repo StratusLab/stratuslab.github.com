c71cd6707887d11505a30b9cca08eae2d688e622
Charles Loomis <cal@sixsq.com>
2012-10-11 14:59:54 +0200

    add pages from old website

---
+++ fp7/lib/exe/opensearch.php
@@ -0,0 +1,7 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+  <ShortName>StratusLab</ShortName>
+  <Image width="16" height="16" type="image/x-icon">http://stratuslab.eu/lib/tpl/default/images/favicon.ico</Image>
+  <Url type="text/html" template="http://stratuslab.eu/doku.php?do=search&amp;id={searchTerms}" />
+  <Url type="application/x-suggestions+json" template="http://stratuslab.eu/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
+</OpenSearchDescription>
